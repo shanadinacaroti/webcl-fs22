782c35117440a55e6055ee01a32f15208f833a40
Dierk Koenig <dierk.koenig@canoo.com>
2022-03-08 15:02:11 +0100

    after week 3

---
+++ week3/jsDoc.js
@@ -0,0 +1,15 @@
+
+
+/** @type { String } */
+let a = "hallo";
+
+// a = "0";
+
+/**
+ * Logs the arg.
+ * @param  { String | Number | void } arg - must be a non-empty.
+ */
+const foo = arg => console.log(arg);
+
+foo("hallo");
+foo("dudu");
